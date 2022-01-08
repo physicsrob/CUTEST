4aadb20c2c90f97589692e0b2e1abb0e73d10502
Rob Porter <physicsrob@gmail.com>
2022-01-08 15:31:21 -0800

    Support disabling VDM.  No longer bitwise exact cuter

--- cuter.asm
+++ cuter.asm
@@ -1,9 +1,11 @@
 	include src/const.asm
-	include src/macros.asm
-	include src/preamble.asm
 	include src/entry.asm
+	include src/macros.asm
+	include src/config_pseudoports.asm
 	include src/console.asm
+	IFDEF USEVDM
 	include src/vdm.asm
+	ENDIF
 	include src/startup.asm
 	include src/command.asm
 	include src/cassette.asm
--- src/config_command.asm
+++ src/config_command.asm
@@ -1,35 +1,9 @@
-;
-;           COMMAND TABLE
-;
-;  THIS TABLE DESCRIBES THE VALID COMMANDS FOR CUTER
-;
-COMTAB:	EQU	$	;START OF KNOWN COMMANDS
-	DB	'DU'	;DUMP
-	DW	DUMP
-	DB	'EN'	;ENTR
-	DW	ENTER
-	DB	'EX'	;EXEC
-	DW	EXEC
-	DB	'GE'	;GET
-	DW	TLOAD
-	DB	'SA'	;SAVE
-	DW	TSAVE
-	DB	'XE'	;XEQ
-	DW	TXEQ
-	DB	'CA'	;CAT
-	DW	TLIST
-	DB	'SE'	;SET COMMAND
-	DW	CSET
-	DB	'CU'	;CUSTOM COMMAND ENTER/CLEAR
-	DW	CUSET
-	DB	0	;END OF TABLE MARK
-
-	ADD_COMMAND 'DU', DUMP, '<addr1> <addr2> - Dump memory data <addr1> to <addr2>'
-	ADD_COMMAND 'EN', ENTER, '<addr> - Enter data into memory starting at <addr>'
-	ADD_COMMAND 'EX', EXEC, '<addr> - Begin program execution at <addr>'
-	ADD_COMMAND 'GE', TLOAD, '(name(/unit) (addr)) - Get a tape file into memory'
-	ADD_COMMAND 'SA', TSAVE, 'name (/unit) <addr1> <addr2> <addr3> - Save a file from memory to tape'
-	ADD_COMMAND 'XE', TXEQ, '(name(/unit) (addr)) - Get then execute a tape file'
-	ADD_COMMAND 'CA', TLIST, '(/unit) Catalog tape files'
-	ADD_COMMAND 'SE', CSET, 'Set'
-	ADD_COMMAND 'CU', CUSET, 'Custom command'
+	ADD_COMMAND 0, 'DU', DUMP, '<addr1> <addr2> - Dump memory data <addr1> to <addr2>'
+	ADD_COMMAND 1, 'EN', ENTER, '<addr> - Enter data into memory starting at <addr>'
+	ADD_COMMAND 2, 'EX', EXEC, '<addr> - Begin program execution at <addr>'
+	ADD_COMMAND 3, 'GE', TLOAD, '(name(/unit) (addr)) - Get a tape file into memory'
+	ADD_COMMAND 4, 'SA', TSAVE, 'name (/unit) <addr1> <addr2> <addr3> - Save a file from memory to tape'
+	ADD_COMMAND 5, 'XE', TXEQ, '(name(/unit) (addr)) - Get then execute a tape file'
+	ADD_COMMAND 6, 'CA', TLIST, '(/unit) Catalog tape files'
+	ADD_COMMAND 7, 'SE', CSET, 'Set'
+	ADD_COMMAND 8, 'CU', CUSET, 'Custom command'
