a157b933023f37bba37c9ded38c5dd03b95921c0
Rob Porter <physicsrob@gmail.com>
2022-01-08 16:32:18 -0800

    More restructuring cleanup

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
-	asl -x -L -cpu 8080 cuter.asm 
-	p2hex cuter.p
+	asl -x -L -cpu 8080 src/cutest.asm -o cutest.p
+	p2hex cutest.p
 	
 	#dd if=/dev/zero of=CUTER13.ROM count=2048 ibs=1
 	#dd if=CUTER13.bin of=CUTER13.ROM conv=notrunc
---
+++ src/cutest.asm
@@ -0,0 +1,13 @@
+	include config/constants.asm
+	include entry.asm
+	include macros.asm
+	include config/pseudoports.asm
+	include config/commands.asm
+	include console.asm
+	IFDEF USEVDM
+	include vdm.asm
+	ENDIF
+	include startup.asm
+	include command/command.asm
+	include cassette.asm
+	include memory.asm
